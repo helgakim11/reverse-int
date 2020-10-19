d7cb08a024e3f14b2a5651ff01075e2fa845c7c8
Helga Maznikava <helga@helgas-air.lan>
2020-10-18 19:01:57 -0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = String(Math.abs(n));
+    result = result.split('').reverse().join('');
+    result = Number(result);
+    return result;
+};
